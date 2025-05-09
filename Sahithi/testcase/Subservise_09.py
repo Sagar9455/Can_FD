5e2c9620d84c55db3a04a3233b7c06e6117a5074
Sagar9455 <yuva.vidyasagar@gmail.com>
2025-05-09 15:59:29 +0530

    j

--- Sahithi/testcase/Subservise_09.py
+++ Sahithi/testcase/Subservise_09.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 
 cdd_file_path = "/home/mobase/Can_FD/Sahithi/KY_MKBD_Diagnostic_Rev01.cdd"
-output_path = "combined_service_subservice_05.xlsx"
+output_path = "combined_service_subservice_06.xlsx"
 
 # Step 1: Extract service info
 service_pattern = r'\(\$(\d{2})\)\s*(.*?)<\/TUV>'
@@ -36,8 +36,8 @@ with open(cdd_file_path, 'r', encoding='utf-8') as file:
         # Extract subservice IDs
         sub_match = re.search(subservice_pattern, line)
         if sub_match and current_service:
-            val = sub_match.group(1)
-            hex_val = f"0x{val}"  # Keep subservice ID in hexadecimal format
+            val = int(sub_match.group(1))
+            hex_val = f"0x{val:02X}"  # Keep subservice ID in hexadecimal format
             combined_data.append({
                 'ServiceID': current_service['ServiceID'],
                 'Service_name': current_service['Service_name'],
